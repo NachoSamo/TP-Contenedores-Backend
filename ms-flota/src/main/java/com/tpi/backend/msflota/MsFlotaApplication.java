8473a428f19019addae169965e21f5c644dd0ccb
Samo notebook <ignasamo2@gmail.com>
2025-10-22 01:07:57 -0300

    create:ms-flota

---
+++ ms-flota/src/main/java/com/tpi/backend/msflota/MsFlotaApplication.java
@@ -0,0 +1,13 @@
+package com.tpi.backend.msflota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFlotaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsFlotaApplication.class, args);
+    }
+
+}
