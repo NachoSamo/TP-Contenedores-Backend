311a88a34856a44eed2086ffd67932813f4cb9d8
Samo notebook <ignasamo2@gmail.com>
2025-10-22 01:06:10 -0300

    create:ms-rutas

---
+++ ms-rutas/src/main/java/com/tpi/backend/msrutas/MsRutasApplication.java
@@ -0,0 +1,13 @@
+package com.tpi.backend.msrutas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRutasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRutasApplication.class, args);
+    }
+
+}
