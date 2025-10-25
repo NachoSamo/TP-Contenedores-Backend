bdf2324e108519b4212dfa6ef623e8003cb4b760
lucia <luquiroga402597@gmail.com>
2025-10-25 16:59:33 -0300

    fix: carpetas controller/repository/service movidas al packeage src

--- ms-flota/src/main/java/com/tpi/backend/msflota/repository/TransportistaRepository.java
+++ ms-flota/src/main/java/com/tpi/backend/msflota/repository/TransportistaRepository.java
@@ -1,6 +1,6 @@
 package com.tpi.backend.msflota.repository;
 
-import com.tpi.backend.msflota.model.Transportista;
+import entities.Transportista;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
